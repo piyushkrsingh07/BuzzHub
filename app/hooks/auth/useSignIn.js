8a06520513a642b6e687a7c3d3c53261ab00c795
PIYUSH KUMAR SINGH <piyush02040@gmail.com>
2025-12-14 09:46:02 +0530

    redefined folder structure and added zod validation

--- app/hooks/auth/useSignIn.js
+++ app/hooks/auth/useSignIn.js
@@ -1,14 +1,22 @@
-import axios from "@/app/config/axiosConfig"
+import { signInRequest } from "@/app/auth/authConfig/SignIn";
+import { useMutation } from "@tanstack/react-query";
 
-export const signInRequest=async({email,password})=>{
-    try{
-       const response=await axios.post('/api/signIn',{
-        email,
-        password,
-   
-       })
-       return response.data
-    }catch(error){
-       throw error.response.data
+
+export const useSignIn=()=>{
+    const {isPending,isSuccess,error,mutate:signInMutation} = useMutation({         //on calling the mutate function the use mutation is going to trigger
+        mutationFn:signInRequest,
+        onSuccess:(data)=>{
+            console.log("Successfully signup",data)
+        },
+        onError:(error)=>[
+            console.error('Failed to sign up')
+        ]
+    })
+
+    return {
+        isPending,
+        isSuccess,
+        error,
+       signInMutation
     }
 }
--- app/hooks/auth/useSignup.js
+++ app/hooks/auth/useSignup.js
@@ -1,6 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 
-import { signUpRequest } from "@/app/(auth)/_components/SignUp";
+import { signUpRequest } from "@/app/auth/authConfig/SignUp";
 
 export const useSignUp=()=>{
     const {isPending,isSuccess,error,mutate:signupMutation} = useMutation({         //on calling the mutate function the use mutation is going to trigger
